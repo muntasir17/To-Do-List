78905edd0dca74492634e5991955018fcc8a5ad8
muntasir17 <166358439+muntasir17@users.noreply.github.com>
2024-10-24 08:49:04 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,61 +1,12 @@
-// let scrollRevealOption = {
-//     distance:"50px",
-//     origin:"left",
-//     duration: 1000,
-// };
-
-// ScrollReveal().reveal(".card-item img",{
-//     ...scrollRevealOption,
-// // origin:"rigth",
-// });
-// //  ScrollReveal().reveal(".card-item .discount",{
-// //      ...scrollRevealOption,
-// //  delay : 400,
-// // });
-
-
-// let btn = document.querySelectorAll(".buy")
-// let counter = document.getElementById("counter");
-// let storeItemsContainer = document.querySelector(".store-items")
-// let count = 0;
-// let totalPrice = 0;
-// btn.forEach((key)=>{
-//     key.addEventListener("click", ()=>{
-// count++;
-// counter.innerText = count;
-// let imageElement = key.previousElementSibling.previousElementSibling.src;
-// let productPrice = key.previousElementSibling.textContent;
-// let card = document.createElement("div");
-// card.className = `store-item`
-// card.innerHTML = `
-//     <img src="${imageElement}" alt="">
-//   <p>${productPrice}</p>
-//   <button class="remove">Remove</button>
-// `;
-// storeItemsContainer.appendChild(card);
-// let pricevalue = parseFloat(productPrice.replace("Price: Taka", ""));
-// totalPrice += pricevalue;
-// document.getElementById("total-amount").textContent = `Total Price ${totalPrice} Taka`;
-//     })
-// })
-// storeItemsContainer.addEventListener("click", (event)=>{
-//     if(event.target.classList.contains("remove")){
-// let productPrice = event.target.previousElementSibling.textContent;
-// let pricevalue = parseFloat(productPrice.replace("Price: Taka", ""));
-// totalPrice -= pricevalue;
-// document.getElementById("total-amount").textContent = `Total Price ${totalPrice} Taka`;
-//         count--;
-//         counter.innerHTML = count;
-// event.target.parentElement.remove();
-//     }
-// });
-
-
 let textContainer = document.querySelector(".text-container");
 let btnEl = document.getElementById("btn");
 let inp = document.querySelector(".inp");
 let stars = document.querySelectorAll(".star");
 btnEl.addEventListener("click", ()=>{
+        if(inp.value.trim() === ""){
+alert("Enter a task");
+return;
+    }
     let textElement = document.createElement("p");
     textElement.innerHTML = inp.value;
     let removeBtn = document.createElement("button");
